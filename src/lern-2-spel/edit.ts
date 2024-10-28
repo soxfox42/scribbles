0f741cd64399704a789335ebd9eb2465738c3d2f
Finn Coffey <finn.coffey@gmail.com>
2024-10-28 11:41:24 +1100

    Add "Lern 2 Spel", a spelling trainer

---
+++ src/lern-2-spel/edit.ts
@@ -0,0 +1,103 @@
+const mainEl = document.querySelector("main") as HTMLButtonElement;
+const newButtonEl = document.getElementById("new") as HTMLButtonElement;
+const saveButtonEl = document.getElementById("save") as HTMLButtonElement;
+
+const errorEl = document.getElementById("error") as HTMLDivElement;
+
+function clearErrors() {
+    errorEl.textContent = "";
+    errorEl.classList.add("hide");
+}
+
+function reportError(error: string) {
+    if (errorEl.textContent == "") {
+        errorEl.innerText = error;
+        errorEl.classList.remove("hide");
+    } else {
+        errorEl.innerText += `\n${error}`;
+    }
+}
+
+function saveWords(): boolean {
+    const words: Record<string, string> = {};
+    const allAlts: string[] = [];
+
+    clearErrors();
+    for (const el of mainEl.children) {
+        const word = (el.querySelector(".word") as HTMLInputElement).value;
+        const alts = (el.querySelector(".alts") as HTMLTextAreaElement).value;
+        const altsList = alts.split("\n").filter((line) => !line.match(/^\s*$/));
+
+        if (word.match(/^\s*$/)) {
+            reportError("Empty word.");
+            return false;
+        }
+
+        if (altsList.length == 0) {
+            reportError(`Word without misspellings: ${word}.`);
+            return false;
+        }
+
+        if (words.hasOwnProperty(word)) {
+            reportError(`Duplicate word "${word}".`);
+            return false;
+        }
+
+        for (const alt of altsList) {
+            if (word == alt) {
+                reportError(`"${word}" is misspelled as itself.`);
+                return false;
+            }
+            if (allAlts.includes(alt)) {
+                reportError(`Duplicate misspelling "${alt}".`);
+                return false;
+            }
+            allAlts.push(alt);
+        }
+
+        words[word] = alts;
+    }
+
+    localStorage.setItem("lern-2-spel-words", JSON.stringify(words));
+
+    return true;
+}
+
+function createWord(title: string = "", quotes: string = "") {
+    const newEl = document.createElement("div");
+    newEl.classList.add("flex", "align-top", "gap");
+    newEl.innerHTML = `
+        <div class="flex vertical stretch">
+            <input type="text" class="word" placeholder="Word">
+            <button class="delete" tabindex="-1">Delete</button>
+        </div>
+        <textarea
+            class="grow alts" rows="5"
+            placeholder="Misspellings, one line each" spellcheck="false"
+        ></textarea>
+    `;
+    (newEl.querySelector(".word") as HTMLInputElement).value = title;
+    (newEl.querySelector(".alts") as HTMLTextAreaElement).value = quotes;
+    newEl.querySelector(".delete")!!.addEventListener("click", () => {
+        if (!confirm("Delete this word?")) return;
+        newEl.remove();
+    });
+    mainEl.appendChild(newEl);
+}
+
+newButtonEl.addEventListener("click", () => {
+    createWord();
+});
+
+saveButtonEl.addEventListener("click", () => {
+    if (saveWords()) {
+        location.href = "index.html";
+    }
+});
+
+const words: Record<string, string> = JSON.parse(localStorage.getItem("lern-2-spel-words") || "{}");
+console.log(words);
+
+for (const [word, alts] of Object.entries(words)) {
+    createWord(word, alts);
+}
---
+++ src/lern-2-spel/index.ts
@@ -0,0 +1,64 @@
+const noWordsEl = document.getElementById("no-words") as HTMLDivElement;
+const gameEl = document.getElementById("game") as HTMLDivElement;
+
+const altEl = document.getElementById("alt") as HTMLDivElement;
+const wordEl = document.getElementById("word") as HTMLInputElement;
+const submitEl = document.getElementById("submit") as HTMLButtonElement;
+
+const rawWords: Record<string, string> = JSON.parse(localStorage.getItem("lern-2-spel-words") || "{}");
+const words: Record<string, string[]> = {};
+
+let state: "guess" | "show" = "guess";
+let selectedWord: string = "";
+
+for (const key of Object.keys(rawWords)) {
+    words[key.toLowerCase()] = rawWords[key].split("\n").map(word => word.trim().toLowerCase()).filter(word => word !== "");
+}
+
+if (Object.keys(words).length != 0) {
+    noWordsEl.classList.add("hide");
+    gameEl.classList.remove("hide");
+    nextRound();
+}
+
+function nextRound() {
+    const allWords = Object.keys(words);
+    const wordIndex = Math.floor(Math.random() * allWords.length);
+    selectedWord = allWords[wordIndex];
+
+    const allAlts = words[selectedWord];
+    const altIndex = Math.floor(Math.random() * allAlts.length);
+    const selectedAlt = allAlts[altIndex];
+
+    altEl.textContent = selectedAlt;
+}
+
+function submit() {
+    const guess = wordEl.value.trim().toLowerCase();
+    if (guess === "") return;
+
+    if (state === "guess") {
+        state = "show";
+        if (guess === selectedWord) {
+            wordEl.classList.add("correct");
+        } else {
+            wordEl.classList.add("incorrect");
+            wordEl.value = selectedWord;
+        }
+        wordEl.disabled = true;
+    } else {
+        state = "guess";
+        wordEl.value = "";
+        wordEl.classList.remove("correct", "incorrect");
+        wordEl.disabled = false;
+        wordEl.focus();
+        nextRound();
+    }
+}
+
+submitEl.addEventListener("click", submit);
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        submit();
+    }
+});
